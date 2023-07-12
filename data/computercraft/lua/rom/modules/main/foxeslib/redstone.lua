e370abc825bc6bd3b2e52deed77bd0a00f4da743
Steven <github@steven-h.net>
2023-07-11 22:27:29 -0400

    Clean up grapha's code

--- data/computercraft/lua/rom/modules/main/foxeslib/redstone.lua
+++ data/computercraft/lua/rom/modules/main/foxeslib/redstone.lua
@@ -1,52 +1,46 @@
 local module = {}
 
 module.testColor = function(value, maskOn, maskOff)
-    return bit.band(value, bit.bor(maskOn, maskOff)) == maskOn
-  end
-  
+  return bit.band(value, bit.bor(maskOn, maskOff)) == maskOn
+end
+
 module.testBundledInput = function(side, maskOn, maskOff)
-    return module.testColor(redstone.getBundledInput(side), maskOn, maskOff)
+  return module.testColor(redstone.getBundledInput(side), maskOn, maskOff)
 end
-  
+
 module.testColour = module.testColor
-  
+
 module.redstoneSides = function()
-    local results = {}
-    for i, side in ipairs(redstone.getSides()) do
-      local power = redstone.getAnalogInput(side)
-      if power > 0 then results[side] = { analog = power } end
-  
-      local clrs = redstone.getBundledInput(side)
-      if clrs ~= 0 then results[side] = { bundled = clrs } end
-    end
-  
-    return results
+  local results = {}
+  for i, side in ipairs(redstone.getSides()) do
+    local power = redstone.getAnalogInput(side)
+    if power > 0 then results[side] = { analog = power } end
+
+    local clrs = redstone.getBundledInput(side)
+    if clrs ~= 0 then results[side] = { bundled = clrs } end
+  end
+
+  return results
 end
 
-module.blastRedstone=function(value)
-  for _,v in pairs(redstone.getSides()) do
-      redstone.setOutput(value)
+module.blastRedstone = function(value)
+  for _, v in pairs(redstone.getSides()) do
+    redstone.setOutput(value)
   end
 end
 
-module.blastAnalogRedstone=function(value)
-  for _,v in pairs(redstone.getSides()) do
-      redstone.setAnalogOutput(value)
+module.blastAnalogRedstone = function(value)
+  for _, v in pairs(redstone.getSides()) do
+    redstone.setAnalogOutput(value)
   end
 end
 
-module.blastAnalogueRedstone=module.blastAnalogRedstone
+module.blastAnalogueRedstone = module.blastAnalogRedstone
 
-module.blastBundledRedstone=function(value)
-  for _,v in pairs(redstone.getSides()) do
-      redstone.setBundledOutput(value)
+module.blastBundledRedstone = function(value)
+  for _, v in pairs(redstone.getSides()) do
+    redstone.setBundledOutput(value)
   end
 end
-
-
-
-
 
 return module
-
-
