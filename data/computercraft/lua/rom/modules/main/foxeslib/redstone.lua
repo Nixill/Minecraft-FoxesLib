54508052f6f521e865724554aea01f11624ad6c3
FennecGrapha <walkingfish10@gmail.com>
2023-07-11 20:22:33 -0600

    preliminary redstone.lua, create utils.lua

---
+++ data/computercraft/lua/rom/modules/main/foxeslib/redstone.lua
@@ -0,0 +1,52 @@
+local module = {}
+
+module.testColor = function(value, maskOn, maskOff)
+    return bit.band(value, bit.bor(maskOn, maskOff)) == maskOn
+  end
+  
+module.testBundledInput = function(side, maskOn, maskOff)
+    return module.testColor(redstone.getBundledInput(side), maskOn, maskOff)
+end
+  
+module.testColour = module.testColor
+  
+module.redstoneSides = function()
+    local results = {}
+    for i, side in ipairs(redstone.getSides()) do
+      local power = redstone.getAnalogInput(side)
+      if power > 0 then results[side] = { analog = power } end
+  
+      local clrs = redstone.getBundledInput(side)
+      if clrs ~= 0 then results[side] = { bundled = clrs } end
+    end
+  
+    return results
+end
+
+glib.blastRedstone=function(value)
+  for _,v in pairs(redstone.getSides()) do
+      redstone.setOutput(value)
+  end
+end
+
+glib.blastAnalogRedstone=function(value)
+  for _,v in pairs(redstone.getSides()) do
+      redstone.setAnalogOutput(value)
+  end
+end
+
+glib.blastAnalogueRedstone=glib.blastAnalogRedstone
+
+glib.blastBundledRedstone=function(value)
+  for _,v in pairs(redstone.getSides()) do
+      redstone.setBundledOutput(value)
+  end
+end
+
+
+
+
+
+return module
+
+
